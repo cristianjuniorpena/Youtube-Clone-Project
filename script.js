9430c056a5057e4eb44764ffa9557492eba0992e
arthur <crafta916@gmail.com>
2026-08-19 14:13:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// const input = document.getElementById('my-input')
+// const msg = document.getElementById("msg")
+// const button = document.getElementById("button")
+
+// var items = [
+//     {
+//         title : "Super Mario World DETONADO",
+//         content : "detonado do clássico jogo de super nintendo"
+//     },
+//     {
+//         title : "Como fazer cannabis usando maizena e javascript",
+//         content : "Tutorial de uma prática receita de vó"
+//     },
+//     {
+//         title : "PodCast a respeito de cálculo e matemática avançada",
+//         content : "PodCast acompanhado do grande mestre Ledo Vaccaro discutindo a respeito de cálculo"
+//     }
+// ]
+
+//     const container = document.getElementById("container")
+
+
+// for (let i of items) {
+//     const card = document.createElement("div");
+
+//     const h2 = document.createElement("h2");
+//     h2.textContent = i.title;
+
+//     const p = document.createElement("p")
+//     p.textContent = i.content
+
+//     card.appendChild(h2)
+//     card.appendChild(p)
+//     card.classList.add("card")  
+//     container.appendChild(card) 
+// }
+
+
+
+
+
+
+
